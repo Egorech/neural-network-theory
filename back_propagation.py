75bd04c08162af3d2536a142e0c091fd346766ac
Егор Тупикин <egor_tupikin02@mail.ru>
2022-07-28 18:14:11 +0300

    back_propagation

---
+++ back_propagation.py
@@ -0,0 +1,7 @@
+#  BACK PROPAGATION - BACK PROPAGATION LEARNING ALGORITHM
+
+"""
+The error back propagation method is a gradient calculation method
+that is used when updating the weights of a multilayer perceptron.
+"""
+
