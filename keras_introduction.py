86b50abcb910a920796bc029d783a022f04b968e
Егор Тупикин <egor_tupikin02@mail.ru>
2022-08-03 23:57:33 +0300

    import

--- keras_introduction.py
+++ keras_introduction.py
@@ -1,1 +1,13 @@
 # KERAS - INSTALLATION AND FIRST INTRODUCTION
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras.layers import Dense
+
+
+
+
